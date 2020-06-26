38bb691705f264f041093aa39527329c3be7dee8
IATECAM - HIDEYUKI <AlanShishid>
2020-06-26 15:01:27 -0400

    characters scripts

---
+++ scripts/char/inimigo.js
@@ -0,0 +1,4 @@
+class Inimigo extends Animation{
+    constructor(matriz, imagem, x, y,  largura, altura, larguraSprite, alturaSprite)
+    super(matriz, imagem, x, y,  largura, altura, larguraSprite, alturaSprite)
+}
