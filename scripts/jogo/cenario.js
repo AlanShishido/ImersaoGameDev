6dd6c0c564fe7f289421b96baf7727d7e7801171
AlanShishido <51389025+AlanShishido@users.noreply.github.com>
2020-06-28 09:47:33 -0400

    game with js

--- scripts/jogo/cenario.js
+++ scripts/jogo/cenario.js
@@ -1,18 +1,19 @@
 class Cenario {
-    constructor(imagem, velocity) {
+    constructor(imagem, velocidade) {
         this.imagem = imagem;
-        this.velocity = velocity;
+        this.velocidade = velocidade;
         this.x1 = 0;
         this.x2 = width;
     }
+
     exibe() {
-        image(this.imagem, this.x1, 0, width, height)
-        image(this.imagem, this.x2, 0, width, height)
+        image(this.imagem, this.x1, 0, width, height);
+        image(this.imagem, this.x2, 0, width, height);
     }
 
     move() {
-        this.x1 = this.x1 - this.velocity;
-        this.x2 = this.x2 - this.velocity;
+        this.x1 = this.x1 - this.velocidade;
+        this.x2 = this.x2 - this.velocidade;
 
         if (this.x1 < -width) {
             this.x1 = width;
--- sketch.js
+++ sketch.js
@@ -1,96 +1,37 @@
-let imgCenario;
-let imgPersonagem;
-let imgInimigo;
-let mscTheme;
-
-let cenario;
-let personagem;
-let inimigo;
-
-const matrizPersonagem = [
-    [0, 0],
-    [220, 0],
-    [440, 0],
-    [660, 0],
-    [0, 270],
-    [220, 270],
-    [440, 270],
-    [660, 270],
-    [0, 540],
-    [220, 540],
-    [440, 540],
-    [660, 540],
-    [0, 810],
-    [220, 810],
-    [440, 810],
-    [660, 810],
-]
-const matrizInimigo = [
-    [0, 0],
-    [104, 0],
-    [208, 0],
-    [312, 0],
-    [0, 104],
-    [104, 104],
-    [208, 104],
-    [312, 104],
-    [0, 208],
-    [104, 208],
-    [208, 208],
-    [312, 208],
-    [0, 312],
-    [104, 312],
-    [208, 312],
-    [312, 312],
-    [0, 416],
-    [104, 416],
-    [208, 416],
-    [312, 416],
-    [0, 520],
-    [104, 520],
-    [208, 520],
-    [312, 520],
-    [0, 624],
-    [104, 624],
-    [208, 624],
-    [312, 624],
-]
-
 function preload() {
-    imgCenario = loadImage('assets/imagens/cenario/floresta.png');
-    imgPersonagem = loadImage('assets/imagens/personagem/correndo.png');
-    imgInimigo = loadImage('assets/imagens/inimigos/gotinha.png');
+    imagemCenario = loadImage('imagens/cenario/floresta.png');
+    imagemTelaInicial = loadImage('imagens/assets/telaInicial.png');
+    imagemGameOver = loadImage('imagens/assets/game-over.png');
+    imagemPersonagem = loadImage('imagens/personagem/correndo.png');
+    imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
+    imagemInimigoVoador = loadImage('imagens/inimigos/gotinha-voadora.png');
+    imagemInimigoGrande = loadImage('imagens/inimigos/troll.png');
 
-    mscTheme = loadSound('assets/sons/trilha_jogo.mp3');
+    fonteTelaInicial = loadFont('imagens/assets/fonteTelaInicial.otf');
+
+    somDoJogo = loadSound('sons/trilha_jogo.mp3');
+    somDoPulo = loadSound('sons/somPulo.mp3');
 }
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    telaInicial = new TelaInicial()
+    jogo = new Jogo()
+    cenas = {
+        telaInicial,
+        jogo
+    }
+    jogo.setup()
+    botaoGerenciador = new BotaoGerenciador('Iniciar', width / 2, height / 2)
 
-    cenario = new Cenario(imgCenario, 3);
-    personagem = new Personagem(matrizPersonagem, imgPersonagem, 0, 110, 135, 220, 270);
-    inimigo = new Inimigo(matrizInimigo, imgInimigo, width - 52, 52, 52, 104, 104, 3);
-    frameRate(30);
-    // mscTheme.play();
-    // mscTheme.loop();
+    frameRate(40)
+    somDoJogo.loop();
+}
 
-    // console.log("setup"); // similar ao start das engines do C#
+function keyPressed() {
+    jogo.keyPressed(key)
 }
 
 function draw() {
-    // background(220);
-    // background(imgCenario)
-    cenario.exibe();
-    cenario.move();
-
-
-    personagem.exibe();
-
-    inimigo.exibe();
-    inimigo.moving();
-
-
-
-    // circle(mouseX, mouseY, 200);
-    // console.log("draw"); // similar ao update das engines do C#
+    cenas[cenaAtual].draw()
 }
